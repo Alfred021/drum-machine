72702f7cb262501a46402b3307ebd9112bd62394
Alfred021 <alfredomedina10@hotmail.com>
2021-01-12 22:26:29 -0300

    Changing css to make the page more responsive

--- src/components/panel.js
+++ src/components/panel.js
@@ -3,14 +3,12 @@ import '../App.css'
 const Panel = ({handlePower, message, power}) => {
     return (
       <div className="col-sm-4">
-        <div className="row-mx-auto">
+        <div className="row-mx-auto align-items-center">
           <div className="col-sm-auto">
-            <div className="d-flex align-items-center" id="display">
-            <p className="text-center message">{message}</p>
-            </div>
+              <p className="text-center message">{message}</p>
           </div>
         </div>
-          <div className="row-mx-auto">
+          <div className="row-mx-auto align-items-center">
             <div className="col-sm-auto">
             <div className="form-check form-switch">
                 <input type="checkbox" className="form-check-input" id="flexSwitchCheckDefault" onChange={handlePower} checked={power}></input>
